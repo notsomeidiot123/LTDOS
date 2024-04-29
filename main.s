cfcb31579ddb422bd07df88c0e433b92befe2d7d
notsomeidiot123 <kiminosei321@gmail.com>
2024-04-29 00:54:11 -0400

    FAT16 BPB and some printing utilities

--- main.s
+++ main.s
@@ -1,1 +1,121 @@
 ;LTDOS by Kimi
+;stack items are 2 bytes wide
+;first function argument is at bp + 4 after enter
+;formatted in fat16 cause i hate fat12
+org 0x7c00
+bits 16
+BPB:
+    jmp short startup
+    nop
+    .OEM: db "LTDOS   "
+    .BPS: dw 512
+    .SPC: db 1
+    .RSC: dw 4
+    .FTC: db 1
+    .RDE: dw 16
+    .SPV: dw 2880 * 2
+    .MDT: db 0xf0
+    .SPF: dw 23
+    .SPT: dw 36
+    .HPD: dw 2
+    .HDN: dd 0
+    .LSC: dd 0
+EBPB:
+    .DNM: db 0
+    .NTF: db 0
+    .SIG: db 0x29
+    .VID: dd 0xaa55
+    .VLS: db "LTDOSBOOTFD"
+    .SID: db "FAT16   "
+startup:
+    mov bp, 0x7a00
+    mov sp, 0x7a00
+    mov [bootdata.bootdisc], dh
+    jmp 0:set_load
+set_load:
+    mov word [0x100], 0
+    mov word [0x100], LTDOS_API_INT
+    mov ax, bootdata.welcome
+    push bootdata.welcome
+    call puts
+    add sp, 2
+    mov ax, 0
+    int 0x80
+    jmp $
+; cdelc: puts(char *string)
+puts:
+    push bp
+    mov bp, sp
+    push si
+    mov si, [bp + 4]
+    
+    mov ah, 0xe
+    .loop:
+        lodsb
+        cmp al, 0
+        je .exit
+        int 0x10
+        jmp .loop
+    .exit:
+        pop si
+        pop bp
+        ret
+
+;cdecl: void putd(unsigned short number, unsigned short base)
+putd:
+    ;set frame
+    push bp
+    mov bp, sp
+    push si
+    push dx
+    push bx
+    push cx
+    xor dx, dx
+    mov ax, [bp + 4]
+    ;if zero, just write zero
+    cmp ax, 0
+    jne .cont
+    mov ah, 0xe
+    mov al, '0'
+    int 0x10
+    jmp .exit
+    .cont:
+    mov bx, ax
+    mov cx, [bp + 6]
+    ;create character buffer
+    mov si, 0x800
+    mov byte [si], 0
+    dec si
+    .loop:
+        xor dx, dx
+        idiv cx
+        add dx, '0'
+        mov [si], dl
+        dec si
+        cmp ax, 0
+        ; jmp $
+        je .exit
+        jmp .loop
+    .exit:
+    ; jmp $
+        inc si
+        push si
+        call puts
+        pop si
+        pop cx
+        pop bx
+        pop dx
+        pop si
+        pop bp
+        ret
+
+LTDOS_API_INT:
+    iret
+
+bootdata:
+    .welcome: db "Welcome to LTDOS by Kimi (Version 0.1.0)", 0xa, 0xd, 0
+    .bootdisc: db 0
+
+times 510 - ($-$$) db 0
+db 0x55, 0xaa
+times (2880 * 1024) - ($ - $$) db 0
