0fa0bc79d8b7ba130a7f7013ebadbf7562867bc6
notsomeidiot123 <kiminosei321@gmail.com>
2024-05-01 23:26:26 -0400

    Fixed disk open bug, working on finding and opening files

--- main.s
+++ main.s
@@ -12,7 +12,7 @@ BPB:
     .SPC: db 1
     .RSC: dw 4
     .FTC: db 1
-    .RDE: dw 16
+    .RDE: dw 512
     .SPV: dw 2880 * 2
     .MDT: db 0xf0
     .SPF: dw 23
@@ -33,6 +33,7 @@ startup:
     mov [bootdata.bootdisc], dh
     jmp 0:set_load
 set_load:
+    cld
     mov word [0x100], 0
     mov word [0x100], LTDOS_API_INT
     mov ax, bootdata.welcome
@@ -140,8 +141,9 @@ data:
     .current_disc: db 0
     .disc_fat_ptr: dw 0xa000
     .disc_bpb_ptr: dw 0x9000
-    .usedsegs_list: dw 0b0000_0000_0000_0001
-    
+    .usedsegs_list: dw 0b0000_0000_0000_0011
+    .err_str: db "An exception has occured. Error ", 0
+    .filename_buffer: times 128 db 0
 
 LTDOS_API_INT:
     iret
@@ -150,6 +152,19 @@ LTDOS_API_INT:
 stage_two:
     push 'A'
     call open_disk
+    cmp ax, 0
+    jne error_halt
+    jmp $
+
+error_halt:
+    push ax
+    push data.err_str
+    call puts
+    add sp, 2
+    pop ax
+    push 10
+    push ax
+    call putd
     jmp $
 
 allocate_cluster:
@@ -207,7 +222,14 @@ open_disk:
     mov es, bx
     mov bx, [data.disc_bpb_ptr]
     int 0x13
+    jc .exit_err
     
+    cmp byte [es:bx + 0x26], 0x29
+    je .read
+    cmp byte [es:bx + 0x26], 0x28
+    jne .exit_err
+    
+    .read:
     push word [es:bx + 0xe]
     call lba_to_chs
     add sp, 2
@@ -215,8 +237,10 @@ open_disk:
     mov ah, 2
     mov al, [es:bx + 0x16]
     mov dl, [bp + 4]
+    sub dl, 'A'
     mov bx, [data.disc_fat_ptr]
     int 0x13
+    jc .exit_err
     mov [data.current_disc], dl
     
     .exit:
@@ -227,6 +251,14 @@ open_disk:
         pop bp
         mov ax, 0
         ret
+    .exit_err:
+        mov ax, 1
+        pop bx
+        pop dx
+        pop es
+        pop cx
+        pop bp
+        ret
 
 open_file:
     ;ARGS:
@@ -235,6 +267,57 @@ open_file:
     ;find file, return struct
     ;ptr + 0 = current cluster index
     ;ptr + 2 = first cluster index
+    push bp
+    mov bp, sp
+    push dx
+    push bx
+    push cx
+    push es
+    push ds
+    push di
+    push si
+    
+    mov bx, [bp + 10]
+    mov ds, bx
+    mov si, [bp + 8]
+    
+    mov di, 0
+    mov es, di
+    mov di, data.filename_buffer
+    
+    push di
+    
+    mov cx, 64
+    rep movsw
+    
+    pop di
+    push di
+    xor cx, cx
+    .tokenize:
+        cmp byte [es:di], 0
+        je .tokenize_end
+        cmp byte [es:di], '/'
+        jne .increment
+        mov byte [es:di], 0
+    .increment:
+        inc di
+        jmp .tokenize
+    .tokenize_end:
+    
+    ;OPEN ROOT DIRECTORY
+    push es
+    
+    xor bx, bx
+    mov es, bx
+    mov bx, data.disc_bpb_ptr
+    mov ax, [es:bx + 0x10]
+    add ax, [es:bx + 0x0e]
+    push ax
+    call lba_to_chs
+    add sp, 2
+    
+    
+
 create_file:
     ;ARGS:
     ;char *file
